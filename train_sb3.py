765261bb5243a8f95a21f46a8a31470e7e14a185
Samuel Barbosa <s.barbosagoncalves@reply.it>
2025-06-28 11:51:46 +0200

    add a testing loop

--- train_sb3.py
+++ train_sb3.py
@@ -7,18 +7,21 @@
 import gym
 from env.custom_hopper import *
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 
 def main():
-    train_env = gym.make('CustomHopper-source-v0')
+    train_env = Monitor(gym.make('CustomHopper-source-v0'))  # Use a Monitor wrapper to log training episodes
     
     print('State space:', train_env.observation_space)  # state-space
     print('Action space:', train_env.action_space)  # action-space
     print('Dynamics parameters:', train_env.get_parameters())  # masses of each link of the Hopper
 
-    eval_env = gym.make('CustomHopper-source-v0')  # Use a separate env for evaluation
+    eval_env = Monitor(gym.make('CustomHopper-source-v0'))  # Use a separate env for evaluation
 
     model = PPO("MlpPolicy", train_env, verbose=1)
+    # model = PPO.load("./best_model/best_model", env=train_env) # Uncomment to load a pre-trained model
+
 
     # Callbacks
     checkpoint_callback = CheckpointCallback(save_freq=5_000, save_path='./checkpoints/',
@@ -32,16 +35,19 @@ def main():
 
     # After training, load and test the best model:
     model = PPO.load("./best_model/best_model")
-    obs = eval_env.reset()
-    done = False
-    total_reward = 0
-    while not done:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, done, info = eval_env.step(action)
-        total_reward += reward
-        # eval_env.render()  # Optional
 
-    print("Test episode reward:", total_reward)
+    n_episodes = 10
+    rewards = []
+    for _ in range(n_episodes):
+        obs = eval_env.reset()
+        done = False
+        total_reward = 0
+        while not done:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, done, info = eval_env.step(action)
+            total_reward += reward
+        rewards.append(total_reward)
+    print("Average test reward:", sum(rewards)/len(rewards))
 
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
